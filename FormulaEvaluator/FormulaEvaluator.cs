c733057a32401883aee76d3cd8d9181f97bdeaad
Rohith0221 <vmrohith@icloud.com>
2024-01-21 14:59:38 -0700

    Fixed few error cases in code
    Added few more tests and fixed implementation of wherever needed

--- FormulaEvaluator/FormulaEvaluator.cs
+++ FormulaEvaluator/FormulaEvaluator.cs
@@ -6,6 +6,7 @@ public static class Evaluator
 {
     public delegate int Lookup(String variable_name);
 
+
     public static int Evaluate(String expression,
                            Lookup variableEvaluator)
     {
@@ -17,156 +18,173 @@ public static class Evaluator
 
         for (int i = 0; i < substrings.Length; i++)
         {
-            /// If token is an integer
-            if (int.TryParse(substrings[i], out int parsedValue))
+            substrings[i] = substrings[i].Trim();
+            if (substrings[i].Equals("") || substrings[i].Equals("+")
+                || substrings[i].Equals("-") || substrings[i].Equals("/")
+                || substrings[i].Equals(")") || substrings[i].Equals("/")
+                || substrings[i].Equals("(") || substrings[i].Equals("*")
+                || isVariable(substrings[i])
+                || (int.TryParse(substrings[i], out int checkConvertedInt) && checkConvertedInt >= 0))
             {
-                /// if operatorStack has either multiplication or division operator, performs right operation
-                /// and pushes result onto valueStack
 
-                if (operatorStack != null && (operatorStack.Count != 0) && ((operatorStack.Peek().Equals("/"))
-                     || (operatorStack.Peek().Equals("*"))))
+                if (substrings[i].Equals(""))
+                    continue;
+
+                /// If token is an integer
+                if (int.TryParse(substrings[i], out int parsedValue))
                 {
-                    if (valueStack.Count != 0)
-                    {
-                        int poppedInt = valueStack.Pop();
-                        string poppedOperator = operatorStack.Pop();
-                        if (poppedOperator.Equals("*"))
-                            valueStack.Push(poppedInt * parsedValue);
-                        else if (poppedOperator.Equals("/"))
-                        {
-                            if (parsedValue == 0)
-                                throw new Exception(" Cannot divide by zero! ");
-                            valueStack.Push(poppedInt / parsedValue);
-                        }
-                    }
-                }
-                else
-                    valueStack.Push(parsedValue);
-            }
-            else if (isVariable(substrings[i]))
-            {
-                try
-                {
-                    if ((operatorStack != null && operatorStack.Count != 0)
-                        && ((operatorStack.Peek().Equals("/"))
-                        || (operatorStack.Peek().Equals("*"))))
+                    /// if operatorStack has either multiplication or division operator, performs right operation
+                    /// and pushes result onto valueStack
+
+                    if (operatorStack != null && (operatorStack.Count != 0) && ((operatorStack.Peek().Equals("/"))
+                         || (operatorStack.Peek().Equals("*"))))
                     {
                         if (valueStack.Count != 0)
                         {
                             int poppedInt = valueStack.Pop();
                             string poppedOperator = operatorStack.Pop();
                             if (poppedOperator.Equals("*"))
-                                valueStack.Push(poppedInt * variableEvaluator(substrings[i]));
+                                valueStack.Push(poppedInt * parsedValue);
                             else if (poppedOperator.Equals("/"))
                             {
-                                if (variableEvaluator(substrings[i]) == 0)
+                                if (parsedValue == 0)
                                     throw new Exception(" Cannot divide by zero! ");
-                                valueStack.Push(poppedInt / variableEvaluator(substrings[i]));
+                                valueStack.Push(poppedInt / parsedValue);
                             }
                         }
                     }
                     else
-                        valueStack.Push(variableEvaluator(substrings[i]));
+                        valueStack.Push(parsedValue);
                 }
-                catch (Exception e)
+                else if (isVariable(substrings[i]))
                 {
-                    Console.WriteLine("No value found for given variable !");
+                    try
+                    {
+                        if ((operatorStack != null && operatorStack.Count != 0)
+                           && ((operatorStack.Peek().Equals("/"))
+                           || (operatorStack.Peek().Equals("*"))))
+                        {
+                            if (valueStack.Count != 0)
+                            {
+                                int poppedInt = valueStack.Pop();
+                                string poppedOperator = operatorStack.Pop();
+                                if (poppedOperator.Equals("*"))
+                                    valueStack.Push(poppedInt * variableEvaluator(substrings[i]));
+                                else if (poppedOperator.Equals("/"))
+                                {
+                                    if (variableEvaluator(substrings[i]) == 0)
+                                        throw new Exception(" Cannot divide by zero! ");
+                                    valueStack.Push(poppedInt / variableEvaluator(substrings[i]));
+                                }
+                            }
+                        }
+                        else
+                            valueStack.Push(variableEvaluator(substrings[i]));
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("No value found for given variable !");
+                    }
+
+                }
+                else if (substrings[i].Equals("+") || substrings[i].Equals("-"))
+                {
+                    if (valueStack.Count >= 2)
+                    {
+                        int value1 = valueStack.Pop();
+                        int value2 = valueStack.Pop();
+                        if (operatorStack.Count != 0 && operatorStack.Peek().Equals("+")
+                            || operatorStack.Peek().Equals("-"))
+                        {
+                            string poppedOperator = operatorStack.Pop();
+                            if (poppedOperator.Equals("+"))
+                                valueStack.Push(value1 + value2);
+                            else if (poppedOperator.Equals("-"))
+                                valueStack.Push(value2 - value1);
+                        }
+                    }
+                    operatorStack.Push(substrings[i]);
                 }
 
-            }
-            else if (substrings[i].Equals("+") || substrings[i].Equals("-"))
-            {
-                if (valueStack.Count >= 2)
+                else if (substrings[i].Equals("*") || substrings[i].Equals("/"))
+                    operatorStack.Push(substrings[i]);
+
+                else if (substrings[i].Equals("("))
+                    operatorStack.Push(substrings[i]);
+
+                // If top of the opeatorStack is ")" 
+                else if (substrings[i].Equals(")"))
                 {
-                    int value1 = valueStack.Pop();
-                    int value2 = valueStack.Pop();
-                    if (operatorStack.Count != 0 && operatorStack.Peek().Equals("+")
-                        || operatorStack.Peek().Equals("-"))
+                    // performs the operation by checking for the errors
+
+                    /// If top of the operatorStack is either "+" or "-"
+                    /// then applies operator to the popped values
+                    if (operatorStack.Count != 0 && valueStack.Count >= 2 &&
+                        operatorStack.Peek().Equals("+") ||
+                        operatorStack.Peek().Equals("-"))
                     {
+                        int value1 = valueStack.Pop();
+                        int value2 = valueStack.Pop();
                         string poppedOperator = operatorStack.Pop();
                         if (poppedOperator.Equals("+"))
                             valueStack.Push(value1 + value2);
                         else if (poppedOperator.Equals("-"))
                             valueStack.Push(value2 - value1);
-                    }
-                }
-                operatorStack.Push(substrings[i]);
-            }
 
-            else if (substrings[i].Equals("*") || substrings[i].Equals("/"))
-                operatorStack.Push(substrings[i]);
+                        // Step 2 if "(" is at top of stack, pops it
+                        if (operatorStack.Peek().Equals("("))
+                            operatorStack.Pop();
 
-            else if (substrings[i].Equals("("))
-                operatorStack.Push(substrings[i]);
-
-            // If top of the opeatorStack is ")" 
-            else if (substrings[i].Equals(")"))
-            {
-                // performs the operation by checking for the errors
-
-                /// If top of the operatorStack is either "+" or "-"
-                /// then applies operator to the popped values
-                if (operatorStack.Count != 0 && valueStack.Count >= 2 &&
-                    operatorStack.Peek().Equals("+") ||
-                    operatorStack.Peek().Equals("-"))
-                {
-                    int value1 = valueStack.Pop();
-                    int value2 = valueStack.Pop();
-                    string poppedOperator = operatorStack.Pop();
-                    if (poppedOperator.Equals("+"))
-                        valueStack.Push(value1 + value2);
-                    else if (poppedOperator.Equals("-"))
-                        valueStack.Push(value2 - value1);
-
-                    // Step 2 if "(" is at top of stack, pops it
-                    if (operatorStack.Peek().Equals("("))
-                        operatorStack.Pop();
-
-                    /// Step 3
-                    /// If top of the operatorStack is either  "*" or "/"
-                    /// applies the poppedOperator to the values
-                    if (operatorStack.Peek().Equals("*") ||
-                        operatorStack.Peek().Equals("/"))
-                    {
-                        if (operatorStack.Count != 0 && valueStack.Count >= 2)
+                        /// Step 3
+                        /// If top of the operatorStack is either  "*" or "/"
+                        /// applies the poppedOperator to the values
+                        if (operatorStack!=null && operatorStack.Count != 0
+                            &&( operatorStack.Peek().Equals("*") ||
+                            operatorStack.Peek().Equals("/")))
                         {
-                            value1 = valueStack.Pop();
-                            value2 = valueStack.Pop();
+                            if (valueStack.Count >= 2)
+                            {
+                                value1 = valueStack.Pop();
+                                value2 = valueStack.Pop();
 
-                            poppedOperator = operatorStack.Pop();
+                                poppedOperator = operatorStack.Pop();
 
-                            if (poppedOperator.Equals("*"))
-                                valueStack.Push(value1 * value2);
-                            else if (poppedOperator.Equals("/"))
-                                if (value1 == 0)
-                                    throw new Exception(" Cannot divide by zero ");
-                            valueStack.Push(value2 / value1);
+                                if (poppedOperator.Equals("*"))
+                                    valueStack.Push(value1 * value2);
+                                else if (poppedOperator.Equals("/"))
+                                    if (value1 == 0)
+                                        throw new Exception(" Cannot divide by zero ");
+                                valueStack.Push(value2 / value1);
+                            }
                         }
                     }
-                }
 
+                }
             }
+            else
+                throw new ArgumentException(" Invalid operator " + substrings[i] + " found !");
 
         }
 
-            if (operatorStack.Count == 0 && valueStack.Count == 1)
-                return valueStack.Pop();
+        if (operatorStack.Count == 0 && valueStack.Count == 1)
+                    return valueStack.Pop();
 
-            else if (operatorStack.Count == 1 && valueStack.Count == 2 &&
-                (operatorStack.Peek().Equals("+") || operatorStack.Peek().Equals("-")))
-            {
-                String poppedOperator = operatorStack.Pop();
-                int value1 = valueStack.Pop();
-                int value2 = valueStack.Pop();
+                else if (operatorStack.Count == 1 && valueStack.Count == 2 &&
+                    (operatorStack.Peek().Equals("+") || operatorStack.Peek().Equals("-")))
+                {
+                    String poppedOperator = operatorStack.Pop();
+                    int value1 = valueStack.Pop();
+                    int value2 = valueStack.Pop();
 
-                if (poppedOperator.Equals("+"))
-                    return value1 + value2;
-                else if (poppedOperator.Equals("-"))
-                    return value2 - value1;
-            }
-
+                    if (poppedOperator.Equals("+"))
+                        return value1 + value2;
+                    else if (poppedOperator.Equals("-"))
+                        return value2 - value1;
+                }
         return 0;
+
     }
+
 
 
     private static Boolean isVariable(String token)
--- FormulaEvaluatorTester/Program.cs
+++ FormulaEvaluatorTester/Program.cs
@@ -1,26 +1,72 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Linq.Expressions;
+using System.Text.RegularExpressions;
 using FormulaEvaluator;
 
 Console.WriteLine("Hello, World!");
 
-Console.WriteLine(Evaluator.Evaluate("(2 +   X1)*   5 + 2", null));
+string var = "X1";
+string var2 = "A7";
+Console.WriteLine(Evaluator.Evaluate("(2+35)*A7", var2 => 8));
+Console.WriteLine(Evaluator.Evaluate(" (2 +X1)*5+2", var => 7));
+
+
+
+Console.WriteLine(Evaluator.Evaluate("(2 +   X1)*   5 + 2", var => 7));
+
 Console.WriteLine(Evaluator.Evaluate("5+5", null));
-Console.WriteLine(Evaluator.Evaluate("2*5", null));
+
 Console.WriteLine(Evaluator.Evaluate("5*2+5", null));
+
 Console.WriteLine(Evaluator.Evaluate("4/2*5", null));
 
 Console.WriteLine(Evaluator.Evaluate("5", null));
 
 try
 {
-    Evaluator.Evaluate(" -A- ", null);
+    Evaluator.Evaluate(" -A- ",null);
 }
-catch (ArgumentException)
+catch (Exception)
 {
     Console.WriteLine("Found error");
-    // write a message saying that your code detected the invalid syntax of the formula
 }
 
-if (Evaluator.Evaluate("5+5", null) == 10)
-    Console.WriteLine("Correct");
+Console.WriteLine(Evaluator.Evaluate("-5", null));
+try
+{
+    Console.WriteLine(Evaluator.Evaluate("()/0", null));
+}
+catch (Exception)
+{
+    Console.WriteLine("Error 1");
+}
+try
+{
+    Console.WriteLine(Evaluator.Evaluate("(0*2)/0", null));
+}
+catch(Exception)
+{
+        Console.WriteLine("Error 2");
+
+}
+
+try
+{
+    Console.WriteLine(Evaluator.Evaluate("2*4/0", null));
+}
+catch(Exception)
+{
+    Console.WriteLine("error 3");
+}
+
+try
+{
+    Console.WriteLine(Evaluator.Evaluate("(2*4)+6/0", null));
+}
+
+catch(Exception)
+{
+    Console.WriteLine("error 4");
+}
+
 
