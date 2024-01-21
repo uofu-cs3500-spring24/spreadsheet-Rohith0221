d60ef679cff0235ef3382f187756e12ab14fa80a
Rohith0221 <vmrohith@icloud.com>
2024-01-20 17:10:35 -0700

    Added first basic test

--- FormulaEvaluatorTester/Program.cs
+++ FormulaEvaluatorTester/Program.cs
@@ -1,5 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using FormulaEvaluator;
+
 Console.WriteLine("Hello, World!");
 
-
+if (Evaluator.Evaluate("5+5",null) == 10)
+    Console.WriteLine("Correct");
 
