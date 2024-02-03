b4e118b8a23d05a509345f702baa0dad48e42c13
Rohith0221 <vmrohith@icloud.com>
2024-02-02 20:50:05 -0700

    added few helper methods
    Added few helper methods to normalise,validate and finished setting up constructor

---
+++ FormulaTests/FormulaTests.cs
@@ -0,0 +1,12 @@
+﻿namespace FormulaTests;
+using SpreadsheetUtilities;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        Formula formula = new("x+y");
+    }
+}
