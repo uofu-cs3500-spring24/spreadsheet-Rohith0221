79e04d9d58e425db20c6e8df88b28ee1c2675fde
Rohith0221 <vmrohith@icloud.com>
2024-02-10 23:22:48 -0700

    Created a new class
    Created cell class to represent cells in a spreadsheet.Finished implementation of the spreadsheet class and started writing tests for spreadsheet class

---
+++ Spreadsheet/Spreadsheet.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using SpreadsheetUtilities;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+
+namespace SS
+{
+	public class Spreadsheet:AbstractSpreadsheet
+	{
+        private DependencyGraph cellDependency;
+        private Dictionary<string, Cell> nonEmptyCells;
+
+        public Spreadsheet()
+		{
+            cellDependency= new();
+            nonEmptyCells = new();
+		}
+
+        public override object GetCellContents(string name)
+        {
+            if (name == null || !validateCellName(name))
+                throw new InvalidNameException();
+
+            if (nonEmptyCells.ContainsKey(name))
+                return nonEmptyCells[name].getCellContent();
+            return null;
+        }
+
+        public override IEnumerable<string> GetNamesOfAllNonemptyCells()
+        {
+            if(nonEmptyCells.Count!=0)
+                return nonEmptyCells.Keys;
+            return new HashSet<string>();
+        }
+
+        public override ISet<string> SetCellContents(string name, double number)
+        {
+            if (name == null || !validateCellName(name))
+                throw new InvalidNameException();
+            HashSet<string> dependents = this.GetDirectDependents(name).ToHashSet();
+            dependents.Add(name);
+            Cell cell = new(name,number);
+            nonEmptyCells.Add(name, cell);
+            cellDependency.AddDependency(name, "");
+            return dependents;
+        }
+
+        public override ISet<string> SetCellContents(string name, string text)
+        {
+            if (name == null || !validateCellName(name))
+                throw new InvalidNameException();
+            else if (text == null)
+                throw new ArgumentNullException();
+
+            HashSet<string> dependents = this.GetDirectDependents(name).ToHashSet();
+            dependents.Add(name);
+            Cell cell = new(name, text);
+            nonEmptyCells.Add(name, cell);
+            cellDependency.AddDependency(name, "");
+            return dependents; 
+        }
+
+        public override ISet<string> SetCellContents(string name, Formula formula)
+        {
+            if (formula == null)
+                throw new ArgumentNullException();
+            else if (name == null || !validateCellName(name))
+                throw new InvalidNameException();
+
+
+            Cell cell = new(name, formula);
+            nonEmptyCells.Add(name, cell);
+            foreach(string dependent in formula.GetVariables())
+                cellDependency.AddDependency(name,dependent);
+            HashSet<string> dependents = this.GetDirectDependents(name).ToHashSet();
+            dependents.Add(name);
+            return dependents;
+        }
+
+        protected override IEnumerable<string> GetDirectDependents(string name)
+        {
+            if (name == null || !validateCellName(name))
+                throw new InvalidNameException();
+            return cellDependency.GetDependents(name);
+        }
+
+        private bool validateCellName(string cellName)
+        {
+            return Regex.IsMatch(cellName, @"^[a-zA-Z_][a-zA-Z0-9_]*$");
+        }
+
+    }
+
+}
+
---
+++ SpreadsheetTests/Spreadsheet.cs
@@ -0,0 +1,26 @@
+﻿namespace SpreadsheetTests;
+using SS;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpreadsheetUtilities;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    [ExpectedException(typeof(InvalidNameException))]
+    public void getCellContents_Exception_NameIsNull()
+    {
+        Spreadsheet spreadsheet = new();
+        spreadsheet.GetCellContents(null);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidNameException))]
+    public void getCellContents_Exception_NameIsInvalid()
+    {
+        Spreadsheet spreadsheet = new();
+        spreadsheet.GetCellContents(" ");
+    }
+
+
+}
